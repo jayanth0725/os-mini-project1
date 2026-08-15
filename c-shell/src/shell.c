bfcf98af52c45e16c7d8e0ed53b0a77fea616750
Jayanth Raveendra <jayanth.raveendra@gmail.com>
2026-08-15 19:03:13 +0530

    add subpart A1 of part A (without loop)

---
+++ c-shell/src/shell.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define PATH_LENGTH 4096
+#define HOST_LENGTH 255
+
+void display_prompt(const char *home){
+    char *username = getenv("USER");
+    if(username == NULL){
+        username = getlogin();
+    }
+
+    char hostname[HOST_LENGTH];
+    if(gethostname(hostname, sizeof(hostname)) != 0){
+        strcpy(hostname, "unknown");
+    }
+
+    char cwd[PATH_LENGTH];
+    if(getcwd(cwd, sizeof(cwd)) == NULL){
+        strcpy(cwd, "unknown");
+    }
+
+    int home_length = strlen(home);
+
+    if(strncmp(cwd, home, home_length) == 0 && (cwd[home_length] == '\0' || cwd[home_length] == '/')){
+        printf("<%s@%s:~%s> ", username, hostname, cwd + home_length);
+    }
+    else{
+        printf("<%s@%s:%s> ", username, hostname, cwd);
+    }
+}
+
+int main(){
+    char shell_home[PATH_LENGTH];
+    if(getcwd(shell_home, sizeof(shell_home)) == NULL){
+        perror("Failed to get starting directory");
+        return 1;
+    }
+    display_prompt(shell_home);
+
+    return 0;
+}
